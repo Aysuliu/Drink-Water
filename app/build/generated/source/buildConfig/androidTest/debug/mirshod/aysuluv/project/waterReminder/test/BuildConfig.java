ade42573ff11aae4f6a6a6b08d304d36eb7a35b8
Aysulu <bakhievaays@gmail.com>
2024-07-27 17:46:17 +0500

    fix test

---
+++ app/build/generated/source/buildConfig/androidTest/debug/mirshod/aysuluv/project/waterReminder/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mirshod.aysuluv.project.waterReminder.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "io.github.z3r0c00l_2k.aquadroid.test";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ app/build/generated/source/buildConfig/debug/mirshod/aysuluv/project/waterReminder/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mirshod.aysuluv.project.waterReminder;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "io.github.z3r0c00l_2k.aquadroid";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 5;
+  public static final String VERSION_NAME = "1.3";
+}
